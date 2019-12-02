8023795b1efe871e6ab222d45913ce0df8cbf940
Harland Lohora <lohoraharland@gmail.com>
2019-12-02 11:05:29 -0600

    Item class

---
+++ item.rb
@@ -0,0 +1,15 @@
+class Item
+    attr_writer :text, :checked
+
+    def initialize( text="" )
+        @text    = text
+        @checked = false
+    end
+    def to_s
+        if @checked 
+            "[X] " + @text 
+        else 
+            "[ ] " + @text
+        end
+    end
+end
